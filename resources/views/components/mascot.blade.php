61eadf5103dbc55a5e9a71eb932a52488cc232d0
Romoli-Gabriele <gh05romoli@gmail.com>
2021-12-22 17:18:42 +0100

    da fixare mascot post

---
+++ resources/views/components/mascot.blade.php
@@ -0,0 +1,15 @@
+<div class="flex items-center lg:justify-center text-sm mt-4">
+    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+    <div class="ml-3 text-left">
+        <h5 class="font-bold">Lary Laracore</h5>
+        <h6>Mascot at Laracasts</h6>
+    </div>
+</div>
+<!--
+<div class="flex items-center text-sm">
+    <img src="./images/lary-avatar.svg" alt="Lary avatar">
+    <div class="ml-3">
+        <h5 class="font-bold">Lary Laracore</h5>
+        <h6>Mascot at Laracasts</h6>
+    </div>
+</div>
